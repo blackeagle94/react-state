b710c707342b900afe153916dc6d0969ddb54f1a
blackeagle94 <blackeagle4894@gmail.com>
2020-12-15 01:37:51 +0200

    installed axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Todos from './components/Todos'
 import Header from './components/layout/Header'
 import AddTodo from './components/AddTodo'
+import About from "./components/pages/About";
 import uuid from 'react-uuid'
 import './App.css'
 
@@ -51,13 +53,20 @@ addTodo = (title) => {
 
   render () {
     return (
-      <div className="App">
+     <Router>
+        <div className="App">
         <div className="container">
           <Header />
-          <AddTodo addTodo={this.addTodo}/>
+          <Route exact path='/' render={props => (
+            <React.Fragment>
+               <AddTodo addTodo={this.addTodo}/>
           <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+            </React.Fragment>
+          )}/>
+          <Route path='/about' component={About} />
         </div>
       </div>
+     </Router>
     )
   }
 }
